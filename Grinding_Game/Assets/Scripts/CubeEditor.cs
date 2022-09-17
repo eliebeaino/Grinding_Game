0a6f24163c3e752cef1a7d45ddd6b4ecfee57c41
eliebeaino <eliebeaino89@gmail.com>
2022-09-17 21:58:41 +0300

    URP v0.1
    URP v0.1:
    - Recreated Project in URP
    - Setup organisation folders inside and outside project(blender projects)
    - Setup ground and cube editor
    - Setup basic test scene
    - Added cinemachine package

---
+++ Grinding_Game/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+
+public class CubeEditor : MonoBehaviour
+{
+    public int gridSize = 4;
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    /// <summary>
+    /// Snaps the Blocks on grid based system according to the set grid size.
+    /// </summary>
+    private void SnapToGrid()
+    {
+        transform.position = new Vector3(
+            GetGridPos().x * gridSize,
+            0,
+            GetGridPos().y * gridSize
+            );
+    }
+    
+    /// <summary>
+    /// Returns Vector 2 int of the current grid position.
+    /// </summary>
+    /// <returns></returns>
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+        Mathf.RoundToInt(transform.position.x / gridSize),
+        Mathf.RoundToInt(transform.position.z / gridSize)
+        );
+    }
+
+    /// <summary>
+    /// Update the Label names of the blocks accordinly to the coordinates
+    /// </summary>
+    private void UpdateLabel()
+    {
+        string coordText = GetGridPos().x + "," + GetGridPos().y;
+
+        //TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        //textMesh.text = coordText;
+
+        gameObject.name = coordText;
+    }
+
+}
